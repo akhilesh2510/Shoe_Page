b0c417de4bcea8ad0ebd41b34d06185340ee9785
akhilesh2510 <akhileshagarwal2510@gmail.com>
2024-03-20 15:56:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navigation from './components/Navigation';
+import MainSection from './components/Hero';
+
+const App=()=>{
+  return ( 
+    <div>
+      <Navigation/>
+      <MainSection/>
+   </div>
+)};
+
+export default App;
+
+
+
